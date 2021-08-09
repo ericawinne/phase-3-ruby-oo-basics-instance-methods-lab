4fd3201008aa04a8e039535caf377980da90fccf
Erica Winne <erica.winne@powerhrg.com>
2021-08-09 09:02:07 -0400

    done yey

---
+++ lib/dog.rb
@@ -0,0 +1,24 @@
+class Dog
+  def bark 
+    puts "Woof!"
+  end
+end
+
+class Dog
+  def bark
+    puts "Woof!"
+  end
+end
+
+fido = Dog.new
+fido.bark
+
+snoopy = Dog.new
+snoopy.bark 
+
+class Dog
+  def sit
+    puts "The Dog is sitting"
+  end
+end
+
